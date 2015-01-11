767751aba7b508c0d871ea14b534d7175b6f30fb
Applier. <appliercontact@gmail.com>
2015-01-11 19:28:57 +0100

    Update Signin.java

--- Signin.java
+++ Signin.java
@@ -1,5 +1,6 @@
 /**
- * 
+ * Signin :
+ * servlet permettant à l'utilisateur de se connecter
  */
 package intergiciels.servlets;
 
@@ -34,12 +35,26 @@ public class Signin extends HttpServlet {
 	public static final String VUE = "/WEB-INF/signin.jsp";
 	public static final String ATTRIBUT_PSEUDO = "pseudo";
     public static final String ATTRIBUT_FORM = "form";
+    public static final String ATTRIBUT_ERREUR = "erreur";
     public static final String ATTRIBUT_SESSION_ID = "id";
     public static final String ATTRIBUT_SESSION_USER = "sessionUser";
     
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		/* Affichage de la page de connexion */
-		this.getServletContext().getRequestDispatcher(VUE).forward(request,response);
+		
+		/* Récupération de la session depuis la requête */
+		HttpSession session = request.getSession();
+		
+		/* Récupération de l'id (mail) de la session en cours */
+		String id = (String) session.getAttribute(ATTRIBUT_SESSION_ID);
+		
+		if (id == null) { // l'utilisateur n'est pas connecté
+			// renvoi vers la page de connexion
+			this.getServletContext().getRequestDispatcher(VUE).forward(request,response);
+		}
+		else { // L'utilisateur est connecté
+			/* renvoi vers la page d'index */
+			this.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request,response);
+		}
 	}
 	
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -53,6 +68,10 @@ public class Signin extends HttpServlet {
         /* Récupération de la session depuis la requete */
         HttpSession session = request.getSession();
         
+        /* Stockage du formulaire et du bean dans l'objet request */
+        request.setAttribute(ATTRIBUT_FORM, form);
+        request.setAttribute(ATTRIBUT_PSEUDO, mail);
+        
         /* Si aucune erreur de validation n'a eu lieu, alors ajout de la String
          * mail à la session, sinon suppression de la session */
         if (facade.userPresent(mail)) {
@@ -60,22 +79,20 @@ public class Signin extends HttpServlet {
         	if (facade.verificationUser(mail, password)) {
         		System.out.println("Vérif mail password OK");
         		session.setAttribute(ATTRIBUT_SESSION_ID, mail);
-        		session.setAttribute(ATTRIBUT_SESSION_USER, user);        		
+        		session.setAttribute(ATTRIBUT_SESSION_USER, user); 
+        		this.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
 	        } else {
 	            session.setAttribute(ATTRIBUT_SESSION_ID, null);
 	            session.setAttribute(ATTRIBUT_SESSION_USER, null);
+	            request.setAttribute(ATTRIBUT_ERREUR, "Echec de la connexion : mot de passe incorrect !");
+	        	this.getServletContext().getRequestDispatcher(VUE).forward( request, response );
 	        }
         }
         else { 
-        	// afficher erreur
-        }
+        	request.setAttribute(ATTRIBUT_ERREUR, "Echec de la connexion : cet email n'est associé à aucun compte !");
+        	this.getServletContext().getRequestDispatcher(VUE).forward( request, response );
+        }   
         
-        
-        /* Stockage du formulaire et du bean dans l'objet request */
-        request.setAttribute(ATTRIBUT_FORM, form);
-        request.setAttribute(ATTRIBUT_PSEUDO, mail);
-        
-        this.getServletContext().getRequestDispatcher(VUE).forward( request, response );
-	}		
+   	}		
 
 }
